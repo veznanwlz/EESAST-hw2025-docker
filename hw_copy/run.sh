48a303ee8d30f79d53c2a54d162a24a36ee72d0a
Weizhou Li <lwz24@mails.tsinghua.edu.cn>
2025-08-12 11:18:52 +0800

    Update run.sh

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,12 @@
-# Add your commands here
+#!/bin/bash
+
+# 检查是否提供了程序名称作为参数
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <program>"
+    exit 1
+fi
+
+program=$1
+
+# 运行指定的程序
+docker run -it --rm hw_copy $program#!/bin/bash
